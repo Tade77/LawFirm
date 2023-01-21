5a139103bf0f5d7dc1da23c5c56f42fe97c1f1e3
Tade77 <adekunletaofeeq@mail.com>
2023-01-22 00:16:59 +0100

    WIP: Responsiveness

--- src/components/Molecules/Logo/Logo.jsx
+++ src/components/Molecules/Logo/Logo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { GoLaw } from "react-icons/go";
+import "./LogoStyle.css";
 const Logo = () => {
   return (
-    <div className=" flex align-middle flex-row-reverse space-x-4">
-      <h1 className="text-[28px] text-[#1c1b1b] ml-4 tracking-widest">
-        Dignity Consultant
-      </h1>
-      <GoLaw className=" w-[45px] h-[45px] fill-blue-700" />
+    <div className="logo--wrapper">
+      <h1 className="logo--desc">Dignity Consultant</h1>
+      <GoLaw className="logo" />
     </div>
   );
 };
